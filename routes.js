c0a0f44379ac7ebe630aa46a1138a6bfd5739ca5
JayeetaMondal <72766760+Jayeetam@users.noreply.github.com>
2022-12-14 13:17:01 -0800

    routes.js

--- routes.js
+++ routes.js
@@ -12,7 +12,6 @@ const requestHandler = (req, res) => {
     res.write('</html>');
     return res.end();
   }
-  
   if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', chunk => {
@@ -37,7 +36,15 @@ const requestHandler = (req, res) => {
   res.end();
 };
 
+// module.exports = requestHandler;
 
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded text'
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some text';
 
 exports.handler = requestHandler;
 exports.someText = 'Some hard coded text';
